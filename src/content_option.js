474d1e8bd82452001658a87f916f359ccf50d810
Harikrishna K <harirkishna.k@piserve.com>
2023-02-09 15:43:31 +0530

    initial commit

--- src/content_option.js
+++ src/content_option.js
@@ -1,39 +1,34 @@
-const logotext = "JOHN";
+const logotext = "SAMSEED";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Mohamed Samseed",
+    description: "I’m Mohamed Samseed Python developer _ Full stack devloper,currently working in Kochi",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Samseed",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
         third: "I develop mobile apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
 const dataabout = {
-    title: "abit about my self",
+    title: "about myself",
     aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
 };
 const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
+        jobtitle: "Associate Software",
+        where: "PiServe",
+        date: "2023",
     },
     {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
+        jobtitle: "Intern",
+        where: "Luminar",
+        date: "2021",
     },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+    
 ];
 
 const skills = [{
@@ -41,12 +36,12 @@ const skills = [{
         value: 90,
     },
     {
-        name: "Djano",
+        name: "Django",
         value: 85,
     },
     {
         name: "Javascript",
-        value: 80,
+        value: 30,
     },
     {
         name: "React",
